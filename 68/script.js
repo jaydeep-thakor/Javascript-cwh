fede8572e539af7c82f3423863d6a55b2821c267
Jaydeep <75525428+jaydeep-thakor@users.noreply.github.com>
2024-11-13 07:32:45 +0530

    Add files via upload

---
+++ 68/script.js
@@ -0,0 +1,18 @@
+// let boxes = document.getElementsByClassName("boxes");
+// Array.from(boxes).forEach((x) => {
+//     x.style.backgroundColor = "red";
+// });
+
+// let box = document.getElementById("box");
+// box.style.backgroundColor = "red";
+
+// let boxes1 = document.querySelectorAll(".boxes");
+// boxes1.forEach((a)=>{
+//     a.innerHTML = "hello"
+// })
+
+// let box1 = document.querySelector(".boxes");
+// box1.innerHTML = "hello"
+
+
+
